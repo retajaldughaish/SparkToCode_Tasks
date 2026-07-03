62d6ab15de4697fe378d3823777460b733a3cd8f
Retaj <retajaldughaishi@gmail.com>
2026-07-04 03:06:04 +0400

    Implemented Problem 2 ( Rectangle Calculator )

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Problem 1 - Personal Info Card
-
+            /*
             String name = "Sara";
             int age = 21;
             double height = 1.65;
@@ -15,7 +15,21 @@
             Console.Write(", Age: " + age);
             Console.Write(", Height: " + height);
             Console.Write(", Student: " + isStudent);
+            */
 
+            ////////////////////////////////////////////////////////
+
+            //Problem 2 - Rectangle Calculator
+
+            Console.Write("Please Enter the Length of a Rectangle: ");
+            float RLength = float.Parse(Console.ReadLine());
+            Console.Write("Please Enter the Width of a Rectangle: ");
+            float RWidth = float.Parse(Console.ReadLine());
+
+            float Area = RLength * RWidth;
+            float Perimeter = 2 * (RLength + RWidth);
+
+            Console.Write("Rectangle Area = " + Area + " , Rectangle Perimeter = " + Perimeter);
         }
     }
 }
