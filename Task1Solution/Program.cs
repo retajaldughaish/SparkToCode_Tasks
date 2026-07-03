f4f1cabb64fccde019c1e982ff8e47fd234789fb
Retaj <retajaldughaishi@gmail.com>
2026-07-04 02:45:22 +0400

    Implemented Problem 1 ( Personal Info Card )

--- Task1Solution/Program.cs
+++ Task1Solution/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Problem 1 - Personal Info Card
+
+            String name = "Sara";
+            int age = 21;
+            double height = 1.65;
+            bool isStudent = true;
+
+            Console.Write("Name: " + name);
+            Console.Write(", Age: " + age);
+            Console.Write(", Height: " + height);
+            Console.Write(", Student: " + isStudent);
+
         }
     }
 }
