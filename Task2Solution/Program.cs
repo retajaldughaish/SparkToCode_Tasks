98764e7540749ee1021ec1be9ddc38b49f4fff80
Retaj <retajaldughaishi@gmail.com>
2026-07-06 02:57:59 +0400

    Implemented Problem 2 ( Sum of Numbers 1 to N )

--- Task2Solution/Program.cs
+++ Task2Solution/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Problem 1 - Countdown Timer
-
+            /*
             Console.Write("Enter the Starting Number for Countdown: ");
             int startingNumber = int.Parse(Console.ReadLine());
 
@@ -14,6 +14,25 @@
                 Console.WriteLine(i);
             }
             Console.WriteLine("Liftoff!");
+            */
+
+            /////////////////////////////////////////////////////////////////////////
+
+            // Problem 2 - Sum of Numbers 1 to N
+
+            Console.Write("Enter a positive integer N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int i;
+
+            int sum = 0;
+
+            for (i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("Sum of numbers from 1 to " + n + " is: " + sum);
         }
     }
 }
