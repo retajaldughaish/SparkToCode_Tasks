acf7d6a9ae06ab590a5388e2f4a1d4e659fa04dd
Retaj <retajaldughaishi@gmail.com>
2026-07-06 02:41:06 +0400

    Implemented Problem 1 ( Countdown Timer )

--- Task2Solution/Program.cs
+++ Task2Solution/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Problem 1 - Countdown Timer
+
+            Console.Write("Enter the Starting Number for Countdown: ");
+            int startingNumber = int.Parse(Console.ReadLine());
+
+            for (int i = startingNumber; i> 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
         }
     }
 }
