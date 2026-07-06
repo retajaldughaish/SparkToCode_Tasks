e078610e1b6ca100b8df6cb1329796b7ae33e7fb
Retaj <retajaldughaishi@gmail.com>
2026-07-06 08:22:29 +0400

    Implemented Problem 2 ( Power & Root Explorer )

--- Task3Solution/Program.cs
+++ Task3Solution/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Problem 1 - Absolute Difference
-
+            /*
             Console.Write("Enter the First Number: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the Second Number: ");
@@ -14,6 +14,20 @@
             int absoluteDifference = Math.Abs(num2 - num1);
 
             Console.Write("The Absolute Difference between {0} and {1} is: {2}", num1, num2, absoluteDifference);
+            */
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Problem 2 - Power & Root Explorer
+
+            Console.Write("Enter the Base Number: ");
+            int baseNum = Convert.ToInt32(Console.ReadLine());
+
+            double Power = Math.Pow(baseNum, 2);
+            double Root = Math.Sqrt(baseNum);
+
+            Console.WriteLine("The Power of {0} is: {1}", baseNum, Power);
+            Console.WriteLine("The Square Root of {0} is: {1}", baseNum, Root);
         }
     }
 }
