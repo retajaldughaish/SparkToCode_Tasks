932c05f0638c2287639d3f16dcc9eab2a2a84f86
Retaj <retajaldughaishi@gmail.com>
2026-07-06 06:46:27 +0400

    Add Project Files

---
+++ Task3Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task3Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
