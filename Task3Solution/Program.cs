71c2b1fcc16c77ea2186efc6e0671705d3aaab85
Retaj <retajaldughaishi@gmail.com>
2026-07-06 08:14:41 +0400

    Implemented Problem 1 ( Absolute Difference )

--- Task3Solution/Program.cs
+++ Task3Solution/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Problem 1 - Absolute Difference
+
+            Console.Write("Enter the First Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Second Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int absoluteDifference = Math.Abs(num2 - num1);
+
+            Console.Write("The Absolute Difference between {0} and {1} is: {2}", num1, num2, absoluteDifference);
         }
     }
 }
