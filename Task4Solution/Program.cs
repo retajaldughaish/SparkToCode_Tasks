fda2282359f836b901610c507c1d1121a6fbf448
Retaj <retajaldughaishi@gmail.com>
2026-07-08 16:11:06 +0400

    Implemented Problem 2 ( Square Number Function )

--- Task4Solution/Program.cs
+++ Task4Solution/Program.cs
@@ -2,18 +2,43 @@
 {
     internal class Program
     {
-        // Problem 1 -  Personalized Welcome Function
-
+        // Problem 1 - Personalized Welcome Function
+        /*
         public static void PrintWelcome (string name)
         {
             Console.WriteLine("Welcome, " + name + "! Have a great day!");
         }
+        */
+
+        ////////////////////////////////////////////////////////////////////////
+
+        // Problem 2 - Square Number Function
+
+        public static int Square (int n)
+        {
+            return n * n;
+        }
+
         static void Main(string[] args)
         {
+            // Problem 1 - Personalized Welcome Function
+            /*
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
 
             PrintWelcome (name);
+            */
+
+            ////////////////////////////////////////////////////////////////////////
+
+            // Problem 2 - Square Number Function
+
+            Console.Write("Enter a Number to be Squared: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            int Result = Square(n);
+
+            Console.WriteLine("Squared Result = " + Result);
         }
     }
 }
