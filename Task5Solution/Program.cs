63e879b0851a9e7d880fb1c8e69301915908dc66
Retaj <retajaldughaishi@gmail.com>
2026-07-11 01:46:45 +0400

    Implemented Problem 2 ( Dynamic To-Do List )

--- Task5Solution/Program.cs
+++ Task5Solution/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             // Problem 1 - Fixed Grades Array
-
+            /*
             int[] StudentGrade = new int[5];
 
             for (int i = 0; i < 5; i++)
@@ -20,6 +20,27 @@
             foreach (int grade in StudentGrade) 
             {
                 Console.WriteLine("Grades: " + grade);
+            }
+            */
+
+            ///////////////////////////////////////////////////////////////////////////
+
+            // Problem 2 - Dynamic To-Do List
+
+            List <string> toDo = new List <string> ();
+
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.Write($"Enter a Task {i + 1} : ");
+                toDo.Add (Console.ReadLine());
+            }
+
+            Console.WriteLine("\n To-Do List");
+            Console.WriteLine();
+
+            foreach (string Task in toDo)
+            {
+                Console.WriteLine("- " + Task);
             }
         }
     }
